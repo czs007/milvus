7fe153d87af947919f0cb1889bd841338aa73ed3
zhenshan.cao <zhenshan.cao@zilliz.com>
2022-05-29 21:45:50 +0800

    ccc
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>

--- scripts/collect_arrow_dep.sh
+++ scripts/collect_arrow_dep.sh
@@ -99,8 +99,8 @@ srcNames=(
 for i in "${!srcNames[@]}"; do
    if test -f "${srcNames[i]}"; then
 	echo "${srcNames[i]} exists. start to copy to ${CUSTOM_THIRDPARTY_PATH}/${targetNames[i]}"
-	echo cp "${srcNames[i]}" -fT  "${CUSTOM_THIRDPARTY_PATH}/${targetNames[i]}"
-	cp "${srcNames[i]}" -fT  "${CUSTOM_THIRDPARTY_PATH}/${targetNames[i]}"
+	echo cp -fT "${srcNames[i]}"  "${CUSTOM_THIRDPARTY_PATH}/${targetNames[i]}"
+	cp  -fT "${srcNames[i]}" "${CUSTOM_THIRDPARTY_PATH}/${targetNames[i]}"
    fi
 done
 
